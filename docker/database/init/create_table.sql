b247dc82cc9437d19f2d6ac35d02202801c6ef7d
Sato0123 <rukajorudano@gmail.com>
2026-06-14 17:28:43 +0900

    zod追加

--- docker/database/init/create_table.sql
+++ docker/database/init/create_table.sql
@@ -1,5 +1,35 @@
-CREATE TABLE IF NOT EXISTS users (
+USE coffee
+
+CREATE TABLE IF NOT EXISTS flavor_categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  code VARCHAR(10) NOT NULL UNIQUE,
+  name VARCHAR(50) NOT NULL
 );
+
+INSERT INTO flavor_categories (code, name) VALUES
+  ('FR', 'フルーティー'),
+  ('FL', 'フローラル'),
+  ('NT', 'ナッティ'),
+  ('CH', 'チョコレート'),
+  ('SP', 'スパイシー'),
+  ('CT', 'シトラス');
+
+CREATE TABLE IF NOT EXISTS roster (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  company_name VARCHAR(100) NOT NULL,
+  company_name2 VARCHAR(100) NOT NULL,
+  user_name VARCHAR(100) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  prefecture VARCHAR(100) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);USE coffee;
+
+CREATE TABLE IF NOT EXISTS coffees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  flavor_category_code VARCHAR(10) NOT NULL,
+  serial_number INT NOT NULL,
+  FOREIGN KEY (flavor_category_code) REFERENCES flavor_categories(code)
+);
+
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,85 @@
-import { useState } from 'react'
+import { useForm, } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+
+import { z } from "zod";
 import './App.css'
 
+
+const RosterSchema = z
+  .object({
+    company_name: z
+      .string()
+      .min(1, "名前は必須です")
+      .max(64, "名前が長すぎます"),
+    company_name2: z
+      .string()
+      .optional(),
+    user_name: z
+      .string()
+      .optional(),
+    address: z
+      .string()
+      .optional(),
+    phone_number: z
+      .string()
+      .optional(),
+    email: z
+      .string()
+      .optional(),
+    prefecture: z
+      .string()
+      .optional(),
+  })
+  ;
+
+type RosterForm = z.infer<typeof RosterSchema>;
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    register,
+    handleSubmit,
+  } = useForm<RosterForm>({
+    resolver: zodResolver(RosterSchema),
+  });
+
+  const onSubmit = async (data: RosterForm) => {
+    const navigate = useNavigate();
+    try {
+      const response = await fetch(`http://127.0.0.1:8001`, {
+        method: 'POST',
+        credentials: "include",
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      if (response.ok) {
+        alert("登録が完了しました。")
+        navigate("/done");
+      } else {
+        alert("登録に失敗しました。")
+      }
+    } catch (error) {
+      console.error("エラー:", error);
+      alert("登録に失敗しました。")
+      return;
+    }
+  };
 
   return (
     <>
       <h1>~Orithankyou Coffee Competition~</h1>
-      <button
-        type="button"
-        className="counter"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        Click count is {count}
-      </button>
+      <form onSubmit={handleSubmit(onSubmit)} style={{ display: "flex", margin: "0 auto", width: "fit-content", flexDirection: "column", maxWidth: "40%" }}>
+        <input {...register("company_name")} placeholder="社名" />
+        <input {...register("company_name2")} placeholder="ふりがな" />
+        <input {...register("user_name")} placeholder="担当者名" />
+        <input {...register("address")} placeholder="住所" />
+        <input {...register("phone_number")} placeholder="電話番号" />
+        <input {...register("email")} placeholder="email" />
+        <input {...register("prefecture")} placeholder="都道府県" />
+
+        <button type="submit">送信</button>
+      </form >
     </>
   )
 }
