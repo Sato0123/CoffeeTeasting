dd2ad27d864161743f0b4e393e75ef046b8035e8
Sato0123 <rukajorudano@gmail.com>
2026-06-14 15:23:23 +0900

    feat: 土台を作成(fastapi,vite(react),mysql)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>~Orithankyou Coffee Competition~</h1>
+      <button
+        type="button"
+        className="counter"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        Click count is {count}
+      </button>
+    </>
+  )
+}
+
+export default App
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from coffeeteasting!")
-
-
-if __name__ == "__main__":
-    main()
